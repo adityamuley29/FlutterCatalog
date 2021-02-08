145e54e9664e1f93c0dcba8a8f6d8c6c0f026ffe
Aditya Muley <adityamuley48@gmail.com>
2021-02-08 21:54:31 +0530

    day6 completed

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import '../utils/routes.dart';
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,7 +25,7 @@ class LogIn extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
@@ -35,9 +43,14 @@ class LogIn extends StatelessWidget {
                 children: [
                   TextFormField(
                     decoration: InputDecoration(
-                        hintText: "Enter Username",
-                        labelText: "Username",
-                        border: new OutlineInputBorder()),
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                      border: new OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   SizedBox(
                     height: 20,
@@ -53,20 +66,53 @@ class LogIn extends StatelessWidget {
                   SizedBox(
                     height: 30,
                   ),
-                  RaisedButton(
-                    padding: EdgeInsets.all(10),
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text(
-                      "LogIn",
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                    ),
-                    onPressed: () {
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: 50,
+                      width: changeButton ? 50 : 120,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ),
+                    ),
                   )
+                  // RaisedButton(
+                  //   padding: EdgeInsets.all(10),
+                  //   color: Colors.blueAccent,
+                  //   textColor: Colors.white,
+                  //   child: Text(
+                  //     "LogIn",
+                  //     style: TextStyle(
+                  //       fontSize: 18,
+                  //     ),
+                  //   ),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
                 ],
               ),
             )
